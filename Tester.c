7228533fd169103919b511b8308ddc48449fbd8c
deepasn08 <105222029+deepasn08@users.noreply.github.com>
2022-08-07 22:56:55 +0530

    Update Tester.c

--- Tester.c
+++ Tester.c
@@ -4,7 +4,7 @@ void Tester()
 {
   assert(batteryIsOk(25, D, 70, 0.7));
   assert(batteryIsOk(35, F, 70, 0.7));
-  assert((batteryIsOk(50, D, 85, 0))==FALSE);
-  assert(batteryIsOk(2.5, F, 85, 0.7)==FALSE);
-  assert(batteryIsOk(10, D, 55, 0.9)==FALSE);
+  assert((batteryIsOk(50, D, 85, 0))==OUTOFRANGE);
+  assert(batteryIsOk(2.5, F, 85, 0.7)==OUTOFRANGE);
+  assert(batteryIsOk(10, D, 55, 0.9)==OUTOFRANGE);
 }
