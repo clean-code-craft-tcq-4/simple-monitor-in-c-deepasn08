2fbe5c81f5ede348e25d490d06709d0d5062ef82
deepasn08 <105222029+deepasn08@users.noreply.github.com>
2022-08-07 22:33:15 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -8,15 +8,6 @@ int batteryIsOk(float temp, enum TEMP_UNIT units, float soc, float cr)
   return  ( IsTempatureOK() &&  isStateOfChargeOK() && isChargeRateinLimit());
 }
 
-void Tester()
-{
-  assert(batteryIsOk(25, D, 70, 0.7));
-  assert(batteryIsOk(35, F, 70, 0.7));
-  assert((batteryIsOk(50, D, 85, 0))==FALSE);
-  assert(batteryIsOk(2.5, F, 85, 0.7)==FALSE);
-  assert(batteryIsOk(10, D, 55, 0.9)==FALSE);
-}
-
 int main()
 {
   Tester();
