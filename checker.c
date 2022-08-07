78b084a2ce7344fdc572ef155cd7ab8510b4a5a7
deepasn08 <105222029+deepasn08@users.noreply.github.com>
2022-08-07 22:27:44 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,7 +1,6 @@
 
 #include "checker.h"
 
-
 int batteryIsOk(float temp, enum TEMP_UNIT units, float soc, float cr)
 {
   temp = ConvertToCelcius(temp, units);
@@ -12,13 +11,9 @@ int batteryIsOk(float temp, enum TEMP_UNIT units, float soc, float cr)
 void Tester()
 {
   assert(batteryIsOk(25, D, 70, 0.7));
-
   assert(batteryIsOk(35, F, 70, 0.7));
-
   assert((batteryIsOk(50, D, 85, 0))==FALSE);
-
   assert(batteryIsOk(2.5, F, 85, 0.7)==FALSE);
-
   assert(batteryIsOk(10, D, 55, 0.9)==FALSE);
 }
 
